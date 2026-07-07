30501fde83511e1cfc3daab3acea7bc7b77c573f
umesh sharma <us61901@gmail.com>
2026-07-07 22:02:48 +0530

    stopwatch fixed

--- src/components/Timerstop.jsx
+++ src/components/Timerstop.jsx
@@ -10,14 +10,13 @@ function Timerstop() {
       setCount((prev) => prev + 1);
     }, 1000);
 
-    return clearInterval(intervelref.current);
+    return () => clearInterval(intervelref.current);
   }, [count]);
   return (
-    <div>
+    <>
       <h1>{count}seconds</h1>
-      <button onClick={clearInterval(intervelref.current)}>stop</button>
-    </div>
+      <button onClick={() => clearInterval(intervelref.current)}>stop</button>
+    </>
   );
 }
-
 export default Timerstop;
