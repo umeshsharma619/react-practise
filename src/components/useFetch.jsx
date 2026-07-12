62c3487d7a4581da82991037bc9d95a5f6b2eee3
umesh sharma <us61901@gmail.com>
2026-07-12 13:31:31 +0530

    uope

--- src/components/useFetch.jsx
+++ src/components/useFetch.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 const useFetch = (url) => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     fetch(url)
